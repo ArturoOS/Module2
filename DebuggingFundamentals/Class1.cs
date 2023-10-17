fc0ec2655ec714722128dbf428a822d0c117ff9d
ArturoOS <Arturo_Osorio1@epam.com>
2023-10-17 14:47:28 -0600

    Add project files.

---
+++ DebuggingFundamentals/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DebuggingFundamentals
+{
+	public class Class1
+	{
+	}
+}
---
+++ Task1/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object newProduct)
+        {
+            // Equals was not working as it should since references are not the same, so I had to 
+            // overwrite Equals method.
+            var product = newProduct as Product;
+            if (product == null) return false;
+            return this.Name == product.Name && this.Price == product.Price;
+        }
+    }
+}
